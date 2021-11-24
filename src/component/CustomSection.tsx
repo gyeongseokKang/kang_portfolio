5794384ec89142a4f2649e2b912606412d023623
kyeungseok_kang <kyeungseok_kang@tmax.co.kr>
2021-11-24 11:08:37 +0900

    [Refactor] 섹션 애니메이션 변경
    - grow -> slide로 변경

--- src/component/CustomSection.tsx
+++ src/component/CustomSection.tsx
@@ -3,6 +3,7 @@ import { styled } from "@mui/material/styles";
 import { NextSeo } from "next-seo";
 import useIntersectionObserver from "src/utils/customHook/useIntersectionObserver";
 import Grow from "@mui/material/Grow";
+import { Slide } from "@mui/material";
 
 const StyledSection = styled("section")({
   maxWidth: "1200px",
@@ -19,7 +20,7 @@ const StyledSection = styled("section")({
 
 interface SectionProp {
   id: string;
-
+  direction?: "up" | "down" | "right" | "left";
   children?: React.ReactNode;
   seo?: {
     title: string;
@@ -27,7 +28,7 @@ interface SectionProp {
   };
 }
 
-export const CustomSection = ({ id, children, seo }: SectionProp) => {
+export const CustomSection = ({ id, children, seo, direction = "up" }: SectionProp) => {
   const containerRef = React.useRef(null);
   const entry = useIntersectionObserver(containerRef, { threshold: 0.1 });
 
@@ -44,13 +45,16 @@ export const CustomSection = ({ id, children, seo }: SectionProp) => {
           }}
         />
       )}
-      <Grow
+      <Slide in={entry?.isIntersecting} direction={direction} timeout={1000}>
+        <div> {children}</div>
+      </Slide>
+      {/* <Grow
         in={entry?.isIntersecting}
         style={{ transformOrigin: "0 0 0" }}
         {...(entry?.isIntersecting ? { timeout: 500 } : {})}
       >
         <div> {children}</div>
-      </Grow>
+      </Grow> */}
       <div style={{ minHeight: "10px" }}></div>
     </StyledSection>
   );
