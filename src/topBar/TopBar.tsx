54738404b1586cfff2c347d5b99dd750bd45bd4a
kyeungseok_kang <kyeungseok_kang@tmax.co.kr>
2021-11-24 23:01:21 +0900

    [Bug fix] 랜더링 관련 에러
    - width에 따른 컴포넌트 랜더링이 이상함(아마도 커스텀훅 에러같음)
    - 일단 useEffect로 한번더 가로채는걸로 임시수정

--- src/topBar/TopBar.tsx
+++ src/topBar/TopBar.tsx
@@ -1,4 +1,5 @@
 import * as React from "react";
+import { useState, useEffect } from "react";
 import AppBar from "@mui/material/AppBar";
 import Toolbar from "@mui/material/Toolbar";
 import Typography from "@mui/material/Typography";
@@ -10,6 +11,15 @@ import TopBarMenu from "./component/TopBarMenu";
 
 export default function TopBar() {
   const { width } = useWindowDimensions();
+  const [menuComponent, setMenuComponent] = useState(<HorizontalMenu />);
+
+  useEffect(() => {
+    if (width > 900) {
+      setMenuComponent(<HorizontalMenu />);
+    } else {
+      setMenuComponent(<VerticalMenu />);
+    }
+  }, [width]);
 
   return (
     <>
@@ -18,34 +28,42 @@ export default function TopBar() {
           <Typography variant="h6" component="div">
             KANG GYEONG SEOK
           </Typography>
-          {width < 900 ? (
-            <Stack justifyContent={"flex-end"} alignItems={"flex-end"} flex={1}>
-              <TopBarMenu>
-                <TopBarSectionItem to={"Intro"} top={false} />
-                <TopBarSectionItem to={"Project"} top={false} />
-                <TopBarSectionItem to={"Skill"} top={false} />
-                <TopBarSectionItem to={"Award"} top={false} />
-                <TopBarSectionItem to={"Certificate"} top={false} />
-                <TopBarSectionItem to={"Blog"} top={false} />
-                <TopBarSectionItem to={"Contact"} top={false} />
-              </TopBarMenu>
-            </Stack>
-          ) : (
-            <Stack direction={"row"} justifyContent={"flex-start"} flex={1}>
-              <div style={{ display: "flex" }}>
-                <TopBarSectionItem to={"Intro"} />
-                <TopBarSectionItem to={"Project"} />
-                <TopBarSectionItem to={"Skill"} />
-                <TopBarSectionItem to={"Award"} />
-                <TopBarSectionItem to={"Certificate"} />
-                <TopBarSectionItem to={"Blog"} />
-                <TopBarSectionItem to={"Contact"} />
-              </div>
-            </Stack>
-          )}
+          {menuComponent}
           <SettingIconDrawer />
         </Toolbar>
       </AppBar>
     </>
   );
 }
+
+const VerticalMenu = () => {
+  return (
+    <Stack justifyContent={"flex-end"} alignItems={"flex-end"} flex={1}>
+      <TopBarMenu>
+        <TopBarSectionItem to={"Intro"} top={false} />
+        <TopBarSectionItem to={"Project"} top={false} />
+        <TopBarSectionItem to={"Skill"} top={false} />
+        <TopBarSectionItem to={"Award"} top={false} />
+        <TopBarSectionItem to={"Certificate"} top={false} />
+        <TopBarSectionItem to={"Blog"} top={false} />
+        <TopBarSectionItem to={"Contact"} top={false} />
+      </TopBarMenu>
+    </Stack>
+  );
+};
+
+const HorizontalMenu = () => {
+  return (
+    <Stack direction={"row"} justifyContent={"flex-start"} alignItems={"flex-start"} flex={1}>
+      <div style={{ display: "flex" }}>
+        <TopBarSectionItem to={"Intro"} />
+        <TopBarSectionItem to={"Project"} />
+        <TopBarSectionItem to={"Skill"} />
+        <TopBarSectionItem to={"Award"} />
+        <TopBarSectionItem to={"Certificate"} />
+        <TopBarSectionItem to={"Blog"} />
+        <TopBarSectionItem to={"Contact"} />
+      </div>
+    </Stack>
+  );
+};
