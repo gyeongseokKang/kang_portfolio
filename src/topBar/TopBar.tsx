0e36eb0faa7c7fea4397c9f6aaaf3ecf1e73c103
kyeungseok_kang <kyeungseok_kang@tmax.co.kr>
2021-11-24 11:28:31 +0900

    [Refactor] Topbar 스타일 수정
    - 크기에 맞춰 padding 조절
    - menuBar로 들어갈 경우 위치조정

--- src/topBar/TopBar.tsx
+++ src/topBar/TopBar.tsx
@@ -2,6 +2,7 @@ import * as React from "react";
 import AppBar from "@mui/material/AppBar";
 import Toolbar from "@mui/material/Toolbar";
 import Typography from "@mui/material/Typography";
+import Stack from "@mui/material/Stack";
 import SettingIconDrawer from "./component/SettingIconDrawer";
 import TopBarSectionItem from "./component/TopBarSectionItem";
 import useWindowDimensions from "src/utils/customHook/useWindowDimensions";
@@ -18,29 +19,34 @@ export default function TopBar() {
             KANG GYEONG SEOK
           </Typography>
           {width > 900 ? (
-            <div style={{ display: "flex", flexGrow: 1 }}>
-              <div style={{ display: "flex", justifyContent: "flex-start" }}>
-                <TopBarSectionItem to={"Intro"} />
-                <TopBarSectionItem to={"Project"} />
-                <TopBarSectionItem to={"Skill"} />
-                <TopBarSectionItem to={"Award"} />
-                <TopBarSectionItem to={"Certificate"} />
-                <TopBarSectionItem to={"Blog"} />
-                <TopBarSectionItem to={"Contact"} />
+            <Stack direction={"row"} justifyContent={"flex-start"} flex={1} alignItems={"center"}>
+              <div style={{ display: "flex", flexGrow: 1, justifyContent: "flex-start" }}>
+                <div style={{ display: "flex" }}>
+                  <TopBarSectionItem to={"Intro"} />
+                  <TopBarSectionItem to={"Project"} />
+                  <TopBarSectionItem to={"Skill"} />
+                  <TopBarSectionItem to={"Award"} />
+                  <TopBarSectionItem to={"Certificate"} />
+                  <TopBarSectionItem to={"Blog"} />
+                  <TopBarSectionItem to={"Contact"} />
+                </div>
               </div>
-            </div>
+              <SettingIconDrawer />
+            </Stack>
           ) : (
-            <TopBarMenu>
-              <TopBarSectionItem to={"Intro"} />
-              <TopBarSectionItem to={"Project"} />
-              <TopBarSectionItem to={"Skill"} />
-              <TopBarSectionItem to={"Award"} />
-              <TopBarSectionItem to={"Certificate"} />
-              <TopBarSectionItem to={"Blog"} />
-              <TopBarSectionItem to={"Contact"} />
-            </TopBarMenu>
+            <Stack direction={"row"} justifyContent={"flex-end"}>
+              <TopBarMenu>
+                <TopBarSectionItem to={"Intro"} top={false} />
+                <TopBarSectionItem to={"Project"} top={false} />
+                <TopBarSectionItem to={"Skill"} top={false} />
+                <TopBarSectionItem to={"Award"} top={false} />
+                <TopBarSectionItem to={"Certificate"} top={false} />
+                <TopBarSectionItem to={"Blog"} top={false} />
+                <TopBarSectionItem to={"Contact"} top={false} />
+              </TopBarMenu>
+              <SettingIconDrawer />
+            </Stack>
           )}
-          <SettingIconDrawer />
         </Toolbar>
       </AppBar>
     </>
--- src/topBar/component/TopBarSectionItem.tsx
+++ src/topBar/component/TopBarSectionItem.tsx
@@ -4,16 +4,16 @@ import { Link } from "react-scroll";
 
 interface TopBarSectionItemProp {
   to: string;
-  onFocus?: boolean;
+  top?: boolean;
 }
 
-export default function TopBarSectionItem({ to, onFocus }: TopBarSectionItemProp) {
+export default function TopBarSectionItem({ to, top = true }: TopBarSectionItemProp) {
   return (
     <Typography
       variant="subtitle1"
       component="div"
       sx={{ flexGrow: 1 }}
-      style={{ minWidth: "75px", textAlign: "center" }}
+      style={{ minWidth: "75px", textAlign: "center", padding: top ? "0.25rem " : "0.25rem 1rem" }}
     >
       <div style={{ cursor: "pointer" }}>
         <Link to={to} spy={true} smooth={true} duration={500} offset={-100}>
