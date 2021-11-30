455dfdd00b392487d3cb0ff2d3419535ebdccdb4
kyeungseok_kang <kyeungseok_kang@tmax.co.kr>
2021-11-30 15:07:05 +0900

    [Refactor] intro section 수정
    - 안에 들어가는 내용 수정

--- src/section/IntroSection.tsx
+++ src/section/IntroSection.tsx
@@ -9,6 +9,14 @@ const CircleImage = styled(Image)({
   borderRadius: "100%",
 });
 
+const StyledTypography = styled(Typography)(({ theme }) => ({
+  display: "inline",
+  background: `linear-gradient(180deg, rgba(0,0,0,0) 50%, ${theme.palette.primary.light} 50%)`,
+  paddingInline: "1px 2px",
+  fontWeight: "500" as any,
+  letterSpacing: "-1px",
+}));
+
 const IntroSection = () => {
   const IntroSectionVAProp: IntroSectionVAProp = {};
   return <IntroSectionView {...IntroSectionVAProp} />;
@@ -28,12 +36,20 @@ const IntroSectionView = ({}: IntroSectionVAProp) => {
       <div style={{ display: "flex", justifyContent: "space-evenly", flexWrap: "wrap" }}>
         <Typography variant="subtitle1" gutterBottom component="div">
           안녕하세요. <strong>강경석</strong>입니다. <br />
-          친구들과 함께 창업한 것을 시작으로, 바쁘게 살고있는 3년차 개발자입니다. <br />
-          다양한 기술과 서비스로 새로운 서비스를 만드는 것을 좋아하며
+          복잡해진 프론트앤드 환경에서 심플함을 찾으려는 3년차 개발자입니다. <br />
+          다양한 기술과 서비스로 혁신적이고 필요한 서비스를 만드는 목표로 삼고 있습니다.
           <br />
-          그럼에도 깊게 공부하는 걸 추구하는 개발자이기도 합니다. <br />
-          읽기 쉬운 코드를 쓰는 개발자, 사랑받는 서비스를 만들고 싶은 개발자가 <br />
-          되고 싶어 오늘도 공부하고 있습니다.
+          <br />
+          <StyledTypography variant="subtitle1" gutterBottom>
+            수정하기 쉬운 코드
+          </StyledTypography>
+          를 만드는
+          <StyledTypography variant="subtitle1" gutterBottom>
+            대체되기 어려운 개발자
+          </StyledTypography>
+          가 되고 싶습니다.
+          <br />
+          글을 쓰는 것도 읽는 것도 좋아하는 개발자가 되고 싶습니다.
         </Typography>
         <CircleImage src={"/images/intro/myface.jpg"} width={250} height={250} priority={true} />
       </div>
