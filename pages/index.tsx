e7bc885b2ae9696386c29cf68e53dd233ca79d86
kyeungseok_kang <kyeungseok_kang@tmax.co.kr>
2021-12-08 09:15:35 +0900

    [Refactor] 최적화 관련 미사용 코드 제거

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,7 +4,6 @@ import TopBar from "../src/topBar/TopBar";
 import IntroSection from "src/section/IntroSection";
 import ExperienceSection from "src/section/ExperienceSection";
 import ScrollToTopBtn from "src/component/button/ScrollToTopBtn";
-import useIntersectionObserver from "src/utils/customHook/useIntersectionObserver";
 import { Suspense } from "react";
 
 const DynamicLazyProjectSection = dynamic(() => import("src/section/ProjectSection"), {
@@ -27,42 +26,6 @@ const DynamicLazyContactSection = dynamic(() => import("src/section/ContactSecti
 });
 
 export default function Index() {
-  const projectRef = React.useRef(null);
-  const skillRef = React.useRef(null);
-  const awardRef = React.useRef(null);
-  const certificateRef = React.useRef(null);
-  const blogRef = React.useRef(null);
-  const contactRef = React.useRef(null);
-
-  const projectEntry = useIntersectionObserver(projectRef, {
-    threshold: 0.1,
-    rootMargin: "100px",
-    freezeOnceVisible: true,
-  });
-  const awardEntry = useIntersectionObserver(awardRef, {
-    threshold: 0.1,
-    rootMargin: "100px",
-    freezeOnceVisible: true,
-  });
-  const skillEntry = useIntersectionObserver(skillRef, {
-    threshold: 0.1,
-    rootMargin: "100px",
-    freezeOnceVisible: true,
-  });
-  const certificateEntry = useIntersectionObserver(certificateRef, {
-    threshold: 0.1,
-    rootMargin: "100px",
-    freezeOnceVisible: true,
-  });
-  const blogEntry = useIntersectionObserver(blogRef, { threshold: 0.1, rootMargin: "100px", freezeOnceVisible: true });
-  const contactEntry = useIntersectionObserver(contactRef, {
-    threshold: 0.1,
-    rootMargin: "100px",
-    freezeOnceVisible: true,
-  });
-
-  console.log("projectEntry", projectEntry);
-  console.log("awardEntry", awardEntry);
   return (
     <>
       <TopBar />
@@ -75,12 +38,6 @@ export default function Index() {
         <DynamicLazyCertificateSection />
         <DynamicLazyBlogSection />
         <DynamicLazyContactSection />
-        {/* <div ref={projectRef}>{projectEntry?.isIntersecting && <DynamicLazyProjectSection />}</div>
-        <div ref={skillRef}>{skillEntry?.isIntersecting && <DynamicLazySkillSection />}</div>
-        <div ref={awardRef}>{awardEntry?.isIntersecting && <DynamicLazyAwardSection />}</div>
-        <div ref={certificateRef}>{certificateEntry?.isIntersecting && <DynamicLazyCertificateSection />}</div>
-        <div ref={blogRef}>{blogEntry?.isIntersecting && <DynamicLazyBlogSection />}</div>
-        <div ref={contactRef}>{contactEntry?.isIntersecting && <DynamicLazyContactSection />}</div> */}
       </Suspense>
       <ScrollToTopBtn />
     </>
