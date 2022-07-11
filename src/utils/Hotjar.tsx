9128ccc1745e964fe1a25c44efb94312360a8922
handy <hk@gaudiolab.com>
2022-07-11 20:41:45 +0900

    [New feature] hotjar 도입 테스트

---
+++ src/utils/Hotjar.tsx
@@ -0,0 +1,24 @@
+import React from "react";
+
+const Hotjar = () => {
+  return (
+    <>
+      <script
+        dangerouslySetInnerHTML={{
+          __html: `
+        (function(h,o,t,j,a,r){
+        h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
+        h._hjSettings={hjid:3060578,hjsv:6};
+        a=o.getElementsByTagName('head')[0];
+        r=o.createElement('script');r.async=1;
+        r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
+        a.appendChild(r);
+    })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');
+                `,
+        }}
+      />
+    </>
+  );
+};
+
+export default Hotjar;
