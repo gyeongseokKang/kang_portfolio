db9278f36a711a19fff58d7ed9591923c2e426f2
Kang Gyeong Seok <rkdrudtjr159@gmail.com>
2022-08-14 15:33:13 +0900

    [Update] 라운드인 관련 정보 업데이트

--- src/section/ProjectSection.tsx
+++ src/section/ProjectSection.tsx
@@ -1,15 +1,19 @@
-import React from "react";
+import CustomTable, {
+  CustomTableCell,
+  CustomTableRow,
+} from "src/component/table/CustomTable";
+
 import { CustomSection } from "../component/CustomSection";
-import IconButton from "@mui/material/IconButton";
-import DescriptionOutlinedIcon from "@mui/icons-material/DescriptionOutlined";
 import { CustomSectionTitle } from "src/component/SectionTitle";
-import CustomTable, { CustomTableCell, CustomTableRow } from "src/component/table/CustomTable";
-import { StackChip } from "src/component/chip/StackChip";
-import { ParticipationChip } from "src/component/chip/ParticipationChip";
-import { Stack } from "@mui/material";
-import { UrlChip } from "src/component/chip/UrlChip";
+import DescriptionOutlinedIcon from "@mui/icons-material/DescriptionOutlined";
 import ExtraInfoDialog from "src/component/dialog/ExtraInfoDialog";
+import IconButton from "@mui/material/IconButton";
+import { ParticipationChip } from "src/component/chip/ParticipationChip";
+import React from "react";
+import { Stack } from "@mui/material";
+import { StackChip } from "src/component/chip/StackChip";
 import { Titleli } from "src/component/ul/Titleli";
+import { UrlChip } from "src/component/chip/UrlChip";
 
 const ProjectSection = () => {
   const ProjectSectionVAProp: ProjectSectionVAProp = {};
@@ -22,12 +26,16 @@ const ProjectSectionView = ({}: ProjectSectionVAProp) => {
     <CustomSection id={"Project"}>
       <CustomSectionTitle
         title={"PROJECT"}
-        subTitle={"새로운 기술, 배우고 싶은 기술을 사용해보고 더 나아진 서비스를 위해 적용합니다"}
+        subTitle={
+          "새로운 기술, 배우고 싶은 기술을 사용해보고 더 나아진 서비스를 위해 적용합니다"
+        }
       />
       <CustomTable
         key="ProjectTable"
         header={["프로젝트", "소개 / 사용기술", "개발 상황", "상세정보"]}
-        footer={"프로젝트는 개발한 순으로 정렬되어 있습니다. 제가 사용한 기술만 적었습니다."}
+        footer={
+          "프로젝트는 개발한 순으로 정렬되어 있습니다. 제가 사용한 기술만 적었습니다."
+        }
       >
         <CustomTableRow key={"골프 레슨 플랫폼 : 라운드인"}>
           <CustomTableCell component="th" scope="row" align="center">
@@ -35,10 +43,24 @@ const ProjectSectionView = ({}: ProjectSectionVAProp) => {
           </CustomTableCell>
           <CustomTableCell>
             <div>[라운드인] 골프 레슨 서비스를 통합하는 플랫폼</div>
-            <StackChip stackList={["React", "Typescript", "Next", "MUI", "Tailwind", "Amplify", "Github"]} />
+            <StackChip
+              stackList={[
+                "React",
+                "Typescript",
+                "Next",
+                "MUI",
+                "Tailwind",
+                "Amplify",
+                "Github",
+              ]}
+            />
           </CustomTableCell>
           <CustomTableCell align="center">
-            <ParticipationChip date={"21/09 ~ CURRENT"} status={"progress"} position={"프론트, 배포"} />
+            <ParticipationChip
+              date={"21/09 ~ CURRENT"}
+              status={"finish"}
+              position={"프론트, 배포"}
+            />
           </CustomTableCell>
           <CustomTableCell align="center">
             <ExtraInfoDialog dialogTitle={"골프 레슨 플랫폼 : 라운드인"}>
@@ -50,11 +72,18 @@ const ProjectSectionView = ({}: ProjectSectionVAProp) => {
               />
               <Titleli
                 title={"서비스 목표"}
-                list={["각종 골프 관련 서비스를 제공할 수 있는 플랫폼", "프로들과 유저들 사이의 1:1 간편 레슨 서비스"]}
+                list={[
+                  "각종 골프 관련 서비스를 제공할 수 있는 플랫폼",
+                  "프로들과 유저들 사이의 1:1 간편 레슨 서비스",
+                ]}
               />
               <Titleli
                 title={"서비스 성과"}
-                list={["레슨 신청 건수, 챌린치 프로그램등 유의미한 참여 증가", "다양한 프로들과 협업 진행중"]}
+                list={[
+                  "레슨 신청 수천건 진행",
+                  "Android, IOS, 웹 모두 제공",
+                  "다양한 프로들과 협업 및 팀 구성",
+                ]}
               />
               <Titleli
                 title={"내 기여도"}
@@ -111,13 +140,29 @@ const ProjectSectionView = ({}: ProjectSectionVAProp) => {
             금융 포트폴리오 플랫폼
           </CustomTableCell>
           <CustomTableCell>
-            <div>[Exfolio] 금융공학 모델 + AI를 이용한 최고의 포트폴리오 생성, 추천 서비스</div>
+            <div>
+              [Exfolio] 금융공학 모델 + AI를 이용한 최고의 포트폴리오 생성, 추천
+              서비스
+            </div>
             <StackChip
-              stackList={["React", "Typescript", "Plotly", "MUI", "Babel", "Webpack", "GithubPage", "Github"]}
+              stackList={[
+                "React",
+                "Typescript",
+                "Plotly",
+                "MUI",
+                "Babel",
+                "Webpack",
+                "GithubPage",
+                "Github",
+              ]}
             />
           </CustomTableCell>
           <CustomTableCell align="center">
-            <ParticipationChip date={"21/03 ~ 21/06"} status={"finish"} position={"프론트, 배포"} />
+            <ParticipationChip
+              date={"21/03 ~ 21/06"}
+              status={"finish"}
+              position={"프론트, 배포"}
+            />
           </CustomTableCell>
           <CustomTableCell align="center">
             <ExtraInfoDialog dialogTitle={"포트폴리오 추천 서비스 : Dr.폴리오"}>
@@ -126,16 +171,32 @@ const ProjectSectionView = ({}: ProjectSectionVAProp) => {
               </video>
               <Titleli
                 title={"서비스 목표"}
-                list={["금융공학 + AI를 이용한 최적의 포트폴리오 추천 서비스", "포트폴리오를 통한 커뮤니티형 플랫폼"]}
+                list={[
+                  "금융공학 + AI를 이용한 최적의 포트폴리오 추천 서비스",
+                  "포트폴리오를 통한 커뮤니티형 플랫폼",
+                ]}
               />
-              <Titleli title={"서비스 성과"} list={["우리은행 해커톤 우수상 수상"]} />
+              <Titleli
+                title={"서비스 성과"}
+                list={["우리은행 해커톤 우수상 수상"]}
+              />
               <Titleli
                 title={"내 기여도"}
-                list={["프론트엔드 개발 및 배포 담당", "서비스 기획 및 디자인", "대화 참가를 위한 사전준비"]}
+                list={[
+                  "프론트엔드 개발 및 배포 담당",
+                  "서비스 기획 및 디자인",
+                  "대화 참가를 위한 사전준비",
+                ]}
               />
               <Stack direction={"row"} justifyContent={"center"}>
-                <UrlChip title={"홈페이지(데모)"} url={"https://dr-folio.github.io/"} />
-                <UrlChip title={"소스코드"} url={"https://github.com/gyeongseokKang/dividend"} />
+                <UrlChip
+                  title={"홈페이지(데모)"}
+                  url={"https://dr-folio.github.io/"}
+                />
+                <UrlChip
+                  title={"소스코드"}
+                  url={"https://github.com/gyeongseokKang/dividend"}
+                />
               </Stack>
             </ExtraInfoDialog>
           </CustomTableCell>
--- src/section/SkillSection.tsx
+++ src/section/SkillSection.tsx
@@ -1,12 +1,16 @@
-import React from "react";
+import CustomTable, {
+  CustomTableCell,
+  CustomTableRow,
+} from "src/component/table/CustomTable";
+
 import { CustomSection } from "../component/CustomSection";
 import { CustomSectionTitle } from "src/component/SectionTitle";
 import Image from "next/image";
-import CustomTable, { CustomTableCell, CustomTableRow } from "src/component/table/CustomTable";
-import Stack from "@mui/material/Stack";
 import Rating from "@mui/material/Rating";
-import { Typography } from "@mui/material";
+import React from "react";
+import Stack from "@mui/material/Stack";
 import { StackChip } from "src/component/chip/StackChip";
+import { Typography } from "@mui/material";
 
 const SkillSection = () => {
   const SkillSectionVAProp: SkillSectionVAProp = {};
@@ -19,28 +23,33 @@ const SkillSectionView = ({}: SkillSectionVAProp) => {
     <CustomSection id={"Skill"}>
       <CustomSectionTitle
         title={"Skill"}
-        subTitle={"빠르게 편하는 최신 기술을 익히고 프로젝트에 적용하고자 고민합니다."}
+        subTitle={
+          "빠르게 편하는 최신 기술을 익히고 프로젝트에 적용하고자 고민합니다."
+        }
       />
-      <CustomTable key="SKillTable" header={["기술", "로고", "사용 경험", "숙련도"]}>
+      <CustomTable
+        key="SKillTable"
+        header={["기술", "-", "사용 경험", "사용 기간"]}
+      >
         <SkillSectionItem
           name={"Javascript"}
           score={3}
           experienceText={`ES6문법이 활발하게 적용될때 배워 이후 버젼도 꾸준히 공부하여 적용하고 있습니다. 
           데이터 시각화 대시보드, 반응형 웹사이트 등을 만들며 활용하고 있습니다.`}
-          careerNumber={3}
+          careerNumber={4}
         />
         <SkillSectionItem
           name={"Typescript"}
           score={3}
           experienceText={`정적 분석을 통해 개발 생산성과 유지보수성이 높아진 것을 실감하여 이후 진행하는 프로젝트 대부분에 적용하고 있습니다.`}
-          careerNumber={2}
+          careerNumber={3}
         />
         <SkillSectionItem
           name={"React"}
           score={3}
           experienceText={`클래스형 컴포넌트에서 함수형 컴포넌트로 대세가 변할때즘 접하고 이후 React의 Flux 패러다임에 매료되어 사용하고 있습니다.
           최신 리액트를 빠르게 따라가고 적용하고 있으며 현재 React18를 도입하여 프로젝트에 사용중입니다.`}
-          careerNumber={2}
+          careerNumber={4}
         />
         <SkillSectionItem
           name={"Next"}
@@ -49,12 +58,14 @@ const SkillSectionView = ({}: SkillSectionVAProp) => {
             React가 최종적으로 가야할 프레임워크라 생각하고 있습니다. 웹뷰 기반, SEO 노출을 위해 Next.js를 도입해서 사용중이며 현재 Next12를 사용하여
             SSR, ISG를 통해 빌드 프로세스를 구축하였습니다.
             `}
-          careerNumber={1}
+          careerNumber={2}
         />
       </CustomTable>
       <Stack direction={"column"}>
         <Stack direction={"row"} padding={0.5}>
-          <Typography sx={{ pr: 2, minWidth: "100px" }}>UI library :</Typography>
+          <Typography sx={{ pr: 2, minWidth: "100px" }}>
+            UI library :
+          </Typography>
           <StackChip stackList={["MUI", "Tailwind", "StyledComponent"]} />
         </Stack>
         <Stack direction={"row"} padding={0.5}>
@@ -62,7 +73,9 @@ const SkillSectionView = ({}: SkillSectionVAProp) => {
           <StackChip stackList={["Jest", "Storybook", "Framer"]} />
         </Stack>
         <Stack direction={"row"} padding={0.5}>
-          <Typography sx={{ pr: 2, minWidth: "100px" }}>Deployment :</Typography>
+          <Typography sx={{ pr: 2, minWidth: "100px" }}>
+            Deployment :
+          </Typography>
           <StackChip stackList={["Amplify", "Vercel", "Githubpage"]} />
         </Stack>
 
@@ -82,14 +95,24 @@ interface SkillSectionItemProp {
   careerNumber: number;
 }
 
-const SkillSectionItem = ({ name, score, experienceText, careerNumber }: SkillSectionItemProp) => {
+const SkillSectionItem = ({
+  name,
+  score,
+  experienceText,
+  careerNumber,
+}: SkillSectionItemProp) => {
   return (
     <CustomTableRow key={name}>
       <CustomTableCell component="th" scope="row" align="center">
         {name}
       </CustomTableCell>
       <CustomTableCell align="center">
-        <Image src={`/icons/${name.toLowerCase()}.svg`} height={50} width={50} alt={name} />
+        <Image
+          src={`/icons/${name.toLowerCase()}.svg`}
+          height={50}
+          width={50}
+          alt={name}
+        />
       </CustomTableCell>
       <CustomTableCell align="center">{experienceText}</CustomTableCell>
       <CustomTableCell align="center">
