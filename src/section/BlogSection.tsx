4f70fb92e787b76b23310dc9dec5841181d64e47
Kang Gyeong Seok <rkdrudtjr159@gmail.com>
2022-09-14 20:02:31 +0900

    [Update] 블로그 관련 내용 업데이트

--- src/section/BlogSection.tsx
+++ src/section/BlogSection.tsx
@@ -1,11 +1,11 @@
-import React from "react";
-import { CustomSection } from "../component/CustomSection";
-import { IconButton } from "@mui/material";
 import ArrowRightIcon from "@mui/icons-material/ArrowRight";
+import { CustomSection } from "../component/CustomSection";
 import { CustomSectionTitle } from "src/component/SectionTitle";
+import { IconButton } from "@mui/material";
 import Image from "next/image";
-import { Stack } from "@mui/material";
 import Link from "next/link";
+import React from "react";
+import { Stack } from "@mui/material";
 
 const BlogSection = () => {
   const BlogSectionVAProp: BlogSectionVAProp = {};
@@ -16,14 +16,19 @@ interface BlogSectionVAProp {}
 const BlogSectionView = ({}: BlogSectionVAProp) => {
   return (
     <CustomSection id={"Blog"}>
-      <CustomSectionTitle title={"Blog"} subTitle={"기억보다 기록을, 그리고 지식공유를 위해 하고 있습니다."} />
+      <CustomSectionTitle
+        title={"Blog"}
+        subTitle={"기억보다 기록을, 그리고 지식공유를 위해 하고 있습니다."}
+      />
       <Stack direction={"row"} flexWrap={"wrap"} justifyContent={"center"}>
         <ul style={{ flex: "1", minWidth: "300px" }}>
           <li>
-            <strong>Tistory</strong> 는 도서 리뷰부터 개발에 대한 모든 것을 씁니다.(일 평균 : 300, 현재 누적 15만)
+            <strong>Tistory</strong> 는 도서 리뷰부터 개발에 대한 모든 것을
+            씁니다.(일 평균 : 600 | 총 288,986(22.09.14 기준))
           </li>
           <li>
-            <strong>Velog</strong> 는 시리즈로 쓰기 위한 글을 씁니다. (산출하기 어려움).
+            <strong>Velog</strong> 는 시리즈로 쓰기 위한 글을 씁니다. (산출하기
+            어려움).
           </li>
         </ul>
         <Stack direction={"row"}>
@@ -32,17 +37,31 @@ const BlogSectionView = ({}: BlogSectionVAProp) => {
           </Stack>
 
           <Stack direction={"row"} alignItems={"center"}>
-            <Link href={"https://all-dev-kang.tistory.com/category/%EA%B0%9C%EB%B0%9C"}>
+            <Link
+              href={
+                "https://all-dev-kang.tistory.com/category/%EA%B0%9C%EB%B0%9C"
+              }
+            >
               <a target="_blank" style={{ textDecoration: "none" }}>
                 <IconButton aria-label="open tistory button">
-                  <Image src={`/icons/tistory.svg`} height={40} width={40} alt={"tistory icon"} />
+                  <Image
+                    src={`/icons/tistory.svg`}
+                    height={40}
+                    width={40}
+                    alt={"tistory icon"}
+                  />
                 </IconButton>
               </a>
             </Link>
             <Link href={"https://velog.io/@kangkyeungseok"}>
               <a target="_blank" style={{ textDecoration: "none" }}>
                 <IconButton aria-label="open velog button">
-                  <Image src={`/icons/velog.png`} height={40} width={40} alt={"velog icon"} />
+                  <Image
+                    src={`/icons/velog.png`}
+                    height={40}
+                    width={40}
+                    alt={"velog icon"}
+                  />
                 </IconButton>
               </a>
             </Link>
