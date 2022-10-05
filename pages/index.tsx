96827f6c65b3f8781dbdb9c1a6a1ca7056d903b2
handy <hk@gaudiolab.com>
2022-10-05 12:06:21 +0900

    [Test] 크롬 브라우저 바로 가기 테스트

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,57 +1,36 @@
 import "../src/locale/i18n";
 
-import * as React from "react";
-
+import dynamic from "next/dynamic";
+import { Suspense } from "react";
+import ScrollToTopBtn from "src/component/button/scrollToTopBtn/ScrollToTopBtn";
 import ExperienceSection from "src/section/ExperienceSection";
 import IntroSection from "src/section/IntroSection";
-import ScrollToTopBtn from "src/component/button/scrollToTopBtn/ScrollToTopBtn";
-import { Suspense } from "react";
 import TopBar from "../src/topBar/TopBar";
-import dynamic from "next/dynamic";
-import { getI18nFromGoogleSheet } from "src/utils/getI18nFromGoogleSheet";
 
-const DynamicLazyProjectSection = dynamic(
-  () => import("src/section/ProjectSection"),
-  {
-    suspense: true,
-  }
-);
-const DynamicLazySkillSection = dynamic(
-  () => import("src/section/SkillSection"),
-  {
-    suspense: true,
-  }
-);
-const DynamicLazyAwardSection = dynamic(
-  () => import("src/section/AwardSection"),
-  {
-    suspense: true,
-  }
-);
-const DynamicLazyCertificateSection = dynamic(
-  () => import("src/section/CertificateSection"),
-  {
-    suspense: true,
-  }
-);
-const DynamicLazyBlogSection = dynamic(
-  () => import("src/section/BlogSection"),
-  {
-    suspense: true,
-  }
-);
-const DynamicLazyContactSection = dynamic(
-  () => import("src/section/ContactSection"),
-  {
-    suspense: true,
-  }
-);
+const DynamicLazyProjectSection = dynamic(() => import("src/section/ProjectSection"), {
+  suspense: true,
+});
+const DynamicLazySkillSection = dynamic(() => import("src/section/SkillSection"), {
+  suspense: true,
+});
+const DynamicLazyAwardSection = dynamic(() => import("src/section/AwardSection"), {
+  suspense: true,
+});
+const DynamicLazyCertificateSection = dynamic(() => import("src/section/CertificateSection"), {
+  suspense: true,
+});
+const DynamicLazyBlogSection = dynamic(() => import("src/section/BlogSection"), {
+  suspense: true,
+});
+const DynamicLazyContactSection = dynamic(() => import("src/section/ContactSection"), {
+  suspense: true,
+});
 
 export default function Index() {
   return (
     <>
       <TopBar />
-      {/* <button onClick={getI18nFromGoogleSheet}>데이터 가져오기</button> */}
+      <a href="https://www.google.com/intl/ko/chrome/">크롬 브라우저로 이동</a>
       <IntroSection />
       <ExperienceSection />
       <Suspense fallback={`loading`}>
