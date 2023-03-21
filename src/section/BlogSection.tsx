f45c22faf4b9a4816d09a9e024697984d3567a7d
gaudiodevops <116620524+gaudiodevops@users.noreply.github.com>
2023-03-21 21:49:11 +0900

    [Modify] 블로그 세션 업데이트

--- src/section/BlogSection.tsx
+++ src/section/BlogSection.tsx
@@ -1,11 +1,9 @@
 import ArrowRightIcon from "@mui/icons-material/ArrowRight";
-import { CustomSection } from "../component/CustomSection";
-import { CustomSectionTitle } from "src/component/SectionTitle";
-import { IconButton } from "@mui/material";
+import { IconButton, Stack } from "@mui/material";
 import Image from "next/image";
 import Link from "next/link";
-import React from "react";
-import { Stack } from "@mui/material";
+import { CustomSectionTitle } from "src/component/SectionTitle";
+import { CustomSection } from "../component/CustomSection";
 
 const BlogSection = () => {
   const BlogSectionVAProp: BlogSectionVAProp = {};
@@ -16,19 +14,11 @@ interface BlogSectionVAProp {}
 const BlogSectionView = ({}: BlogSectionVAProp) => {
   return (
     <CustomSection id={"Blog"}>
-      <CustomSectionTitle
-        title={"Blog"}
-        subTitle={"기억보다 기록을, 그리고 지식공유를 위해 하고 있습니다."}
-      />
+      <CustomSectionTitle title={"Blog"} subTitle={"기억보다 기록을, 그리고 지식공유를 위해 하고 있습니다."} />
       <Stack direction={"row"} flexWrap={"wrap"} justifyContent={"center"}>
         <ul style={{ flex: "1", minWidth: "300px" }}>
           <li>
-            <strong>Tistory</strong> 는 도서 리뷰부터 개발에 대한 모든 것을
-            씁니다.(일 평균 : 600 | 총 288,986(22.09.14 기준))
-          </li>
-          <li>
-            <strong>Velog</strong> 는 시리즈로 쓰기 위한 글을 씁니다. (산출하기
-            어려움).
+            <strong>Tistory</strong> 는 도서 리뷰부터 개발에 대한 모든 것을 씁니다.(월 평균 : 2만 | 총 40만(23.3 기준))
           </li>
         </ul>
         <Stack direction={"row"}>
@@ -37,23 +27,14 @@ const BlogSectionView = ({}: BlogSectionVAProp) => {
           </Stack>
 
           <Stack direction={"row"} alignItems={"center"}>
-            <Link
-              href={
-                "https://all-dev-kang.tistory.com/category/%EA%B0%9C%EB%B0%9C"
-              }
-            >
+            <Link href={"https://all-dev-kang.tistory.com/category/%EA%B0%9C%EB%B0%9C"}>
               <a target="_blank" style={{ textDecoration: "none" }}>
                 <IconButton aria-label="open tistory button">
-                  <Image
-                    src={`/icons/tistory.svg`}
-                    height={40}
-                    width={40}
-                    alt={"tistory icon"}
-                  />
+                  <Image src={`/icons/tistory.svg`} height={40} width={40} alt={"tistory icon"} />
                 </IconButton>
               </a>
             </Link>
-            <Link href={"https://velog.io/@kangkyeungseok"}>
+            {/* <Link href={"https://velog.io/@kangkyeungseok"}>
               <a target="_blank" style={{ textDecoration: "none" }}>
                 <IconButton aria-label="open velog button">
                   <Image
@@ -64,7 +45,7 @@ const BlogSectionView = ({}: BlogSectionVAProp) => {
                   />
                 </IconButton>
               </a>
-            </Link>
+            </Link> */}
           </Stack>
         </Stack>
       </Stack>
