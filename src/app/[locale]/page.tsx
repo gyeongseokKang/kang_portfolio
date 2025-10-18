602cc42dfe9c448de99860581c8d546d63022604
handy kang <handy.kang.dev@gmail.com>
2025-10-18 13:49:30 +0900

    feat: add motion package and ContainerTextFlip component with aceternity registry

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -1,16 +1,16 @@
-"use client";
-
+import { ContainerTextFlip } from "@/components/ui/container-text-flip";
 import { BlogInfoItem } from "@/feature/portfolio/ui/blog-info-item";
 import { CareerBreadcrumb } from "@/feature/portfolio/ui/career-breadcrumb";
-import { useTranslations } from "next-intl";
 
 export default function Home() {
-  const t = useTranslations("HomePage");
   return (
     <div className="container mx-auto p-8 max-w-4xl gap-4 flex flex-col">
-      <h1 className="text-4xl font-bold mb-8 text-center">{t("title")}</h1>
       <CareerBreadcrumb />
       <BlogInfoItem />
+      <ContainerTextFlip
+        className="text-4xl border"
+        words={["TypeScript", "JavaScript", "React.js", "Next.js"]}
+      />
     </div>
   );
 }
