a5224c68ab444fb96115c16c01975bdf343a9dd7
handy kang <handy.kang.dev@gmail.com>
2025-10-18 13:22:48 +0900

    style: update blog info item UI with smaller icons and secondary badges

--- src/feature/portfolio/ui/blog-info-item.tsx
+++ src/feature/portfolio/ui/blog-info-item.tsx
@@ -22,18 +22,18 @@ export function BlogInfoItem() {
           className="group"
         >
           <ItemMedia>
-            <Rss className="size-5" />
+            <Rss className="size-4" />
           </ItemMedia>
           <ItemContent>
             <ItemTitle>편리함을 추구하는 핸디의 지식 블로그</ItemTitle>
-            <ItemDescription>
-              <Badge variant="outline">#Blog</Badge>
-              <Badge variant="outline">#0.7M View</Badge>
-              <Badge variant="outline">#400 posts</Badge>
+            <ItemDescription className="space-x-1">
+              <Badge variant="secondary">#Blog</Badge>
+              <Badge variant="secondary">#0.7M View</Badge>
+              <Badge variant="secondary">#400 posts</Badge>
             </ItemDescription>
           </ItemContent>
           <ItemActions>
-            <ChevronRightIcon className="size-4 group-hover:translate-x-2 transition-all group-hover:size-6" />
+            <ChevronRightIcon className="size-4 group-hover:translate-x-2 transition-all group-hover:size-5" />
           </ItemActions>
         </Link>
       </Item>
