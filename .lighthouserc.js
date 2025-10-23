7ced68afeb1b00e1f090d395296c8b62c65f8dc2
handy kang <handy.kang.dev@gmail.com>
2025-10-23 22:37:43 +0900

    config: update Lighthouse score thresholds with custom category assertions

--- .lighthouserc.js
+++ .lighthouserc.js
@@ -7,7 +7,12 @@ module.exports = {
       url: ['http://localhost:3000/'],
     },
     assert: {
-      preset: 'lighthouse:recommended',
+      assertions: {
+        'categories:performance': ['warn', { minScore: 0.7 }],
+        'categories:accessibility': ['warn', { minScore: 0.8 }],
+        'categories:best-practices': ['error', { minScore: 0.9 }],
+        'categories:seo': ['error', { minScore: 0.9 }],
+      }
     },
     upload: {
       target: 'temporary-public-storage',
